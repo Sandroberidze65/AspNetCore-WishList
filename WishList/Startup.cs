62586f8a469ecb60e7db42263c3b46a6dd3aa3f2
Aleksandre Beridze <sandro.beridze65@gmail.com>
2021-11-23 04:11:28 +0400

    rename Endpoints

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,7 +32,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            app.UseEndpoints(endpoin => { endpoin.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
